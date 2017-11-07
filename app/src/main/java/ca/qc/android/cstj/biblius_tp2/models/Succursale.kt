d660844e18541f7d2afae51a8fefef39a03fb0eb
Maxime Paré <0556559@cstj.qc.ca>
2017-11-07 12:28:35 -0500

    succursale fix

--- app/src/main/java/ca/qc/android/cstj/biblius_tp2/models/Succursale.kt
+++ app/src/main/java/ca/qc/android/cstj/biblius_tp2/models/Succursale.kt
@@ -7,8 +7,16 @@ import com.github.kittinunf.fuel.android.core.Json
  */
 class Succursale(jsonObject: Json) : Item() {
     override fun getAffichage() : String {
-        return nom
+        return appelatif
     }
 
-    var nom : String = jsonObject.obj().getString("appelatif")
+    var appelatif : String = jsonObject.obj().getString("appelatif")
+    var href : String = jsonObject.obj().getString("href")
+    var adresse : String = jsonObject.obj().getString("adresse")
+    var telephone : String = jsonObject.obj().getString("telephone")
+    var telecopieur : String = jsonObject.obj().getString("telecopieur")
+    var information : String = jsonObject.obj().getString("information")
+    var ville : String = jsonObject.obj().getString("ville")
+    var codePostal : String = jsonObject.obj().getString("codePostal")
+    var province : String = jsonObject.obj().getString("province")
 }
